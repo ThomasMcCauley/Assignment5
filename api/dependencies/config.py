2cb52ade70a79a01b76b070614296133913d82a0
ThomasMcCauley <tmccaul4@uncc.edu>
2024-07-14 22:56:45 -0400

    Initial commit

---
+++ api/dependencies/config.py
@@ -0,0 +1,7 @@
+class conf:
+    host = "localhost"
+    database = "sandwich_maker_api"
+    port = 3306
+    user = "tmccaul4"
+    password = "1382320"
+    DATABASE_URL = "mysql+pymysql://tmccaul4:1382320@localhost/sandwich_maker_api"
